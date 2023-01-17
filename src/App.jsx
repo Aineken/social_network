f3b78b4850db0c0ebfc4b0a9acec91a99aa3bd38
andrzul <aineken93@gmail.com>
2023-01-17 22:13:47 +0100

    styled added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 function App() {
   return (
-   <h1>hello world!</h1>
+   <h1>Hello world!</h1>
   )
 }
 
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,22 +1,21 @@
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   // Temp fonts
   fonts: {
-    title: "Roboto, sans-serif",
-    main: "Roboto, sans-serif",
+    title: "Source Sans Pro, sans-serif",
   },
   // Colors for layout
   colors: {
-    primary1: "#fff",
-    background1: "#181D31",
-    accent1: "#F6F6C9",
-    button: "#181D31",
+    primary: "#0f141a",
+    button: "#1d9bf0",
+    buttonText:"#fff",
+    hover:"#8fcdf7",
+    background: "#fff",
   },
   // Breakpoints for responsive design
   breakpoints: {
     sm: "screen and (max-width: 580px)",
     md: "screen and (max-width: 768px)",
     lg: "screen and (max-width: 1024px)",
-    xl: "screen and (max-width: 1280px)",
+    xl: "screen and (max-width: 1400px)",
   },
 };
