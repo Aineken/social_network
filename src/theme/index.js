7de9cce22b7d3d4d0d988306db9ed197a14c981e
andrzul <aineken93@gmail.com>
2023-01-17 21:07:44 +0100

    preparing the files for creating

---
+++ src/theme/index.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  // Temp fonts
+  fonts: {
+    title: "Roboto, sans-serif",
+    main: "Roboto, sans-serif",
+  },
+  // Colors for layout
+  colors: {
+    primary1: "#fff",
+    background1: "#181D31",
+    accent1: "#F6F6C9",
+    button: "#181D31",
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    sm: "screen and (max-width: 580px)",
+    md: "screen and (max-width: 768px)",
+    lg: "screen and (max-width: 1024px)",
+    xl: "screen and (max-width: 1280px)",
+  },
+};
